1b2fbccf031fe49e48b51f051701b8efac830b15
Andrei Minescu <andreiminescu@gmail.com>
2023-08-16 23:12:37 +0300

    Add theme

--- web-bazaar/app/layout.tsx
+++ web-bazaar/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ThemeRegistry from "./theme-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeRegistry options={{ key: "mui" }}>{children}</ThemeRegistry>
+      </body>
     </html>
   );
 }
