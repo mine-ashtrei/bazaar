05ba18e9d49b89b7dd082361d88680dc49fa420d
Andrei Minescu <andreiminescu@gmail.com>
2023-09-16 20:34:18 +0300

    Mui Categories Card

--- next-bazaar/components/categories/card.tsx
+++ next-bazaar/components/categories/card.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
-import Link from "next/link";
+import NextLink from "next/link";
 import { Category } from "../../lib/categories";
+import { Box, Typography, Link as MuiLink } from "@mui/material";
 
 export default function CategoryCard({ item }: { item: Category }) {
+  const categoryUrl = "#";
   return (
-    // <Link href={`/path-to-page/${category.name}`}>
-    <div className="flex items-start flex-col justify-start">
-      <Link href="#" passHref>
-        <div className="cursor-pointer hover:opacity-90 transition">
-          <Image src={item.imgUrl} width={245} height={309} alt={item.name} />
-        </div>
-      </Link>
-      <div className="prose self-center">
-        <h3>{item.name}</h3>
-      </div>
-    </div>
+    <Box
+      sx={{
+        "&:hover img": {
+          opacity: 0.8,
+        },
+        "&:hover a": {
+          textDecoration: "underline",
+        },
+        transition: "opacity 0.4s",
+      }}
+    >
+      <MuiLink component={NextLink} href={categoryUrl}>
+        <Image src={item.imgUrl} width={245} height={309} alt={item.name} />
+      </MuiLink>
+      <MuiLink component={NextLink} href={categoryUrl} underline="hover">
+        <Typography variant="h6" align="center">
+          {item.name}
+        </Typography>
+      </MuiLink>
+    </Box>
   );
 }
