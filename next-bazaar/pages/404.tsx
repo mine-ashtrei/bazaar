a52877abc4db0cdd241b7246e9f8a5c5a39a78fb
Andrei Minescu <andreiminescu@gmail.com>
2023-09-18 18:08:19 +0300

    Update 404 page

--- next-bazaar/pages/404.tsx
+++ next-bazaar/pages/404.tsx
@@ -1,3 +1,10 @@
-export default function NotFoundPage() {
-  return <div>Not found</div>;
-}
+import { Box } from "@mui/material";
+import MinimalLayout from "../components/layouts/minimalLayout";
+
+const NotFoundPage = () => {
+  return <Box sx={{ alignSelf: "center" }}>Not found</Box>;
+};
+
+NotFoundPage.layout = MinimalLayout;
+
+export default NotFoundPage;
