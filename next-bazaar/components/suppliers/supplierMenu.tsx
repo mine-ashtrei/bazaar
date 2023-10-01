c34a75eaaaaf05139101bc650fff61402530cb36
Andrei Minescu <andreiminescu@gmail.com>
2023-10-01 15:12:29 +0300

    Fix reload of dashboard menus

--- next-bazaar/components/suppliers/supplierMenu.tsx
+++ next-bazaar/components/suppliers/supplierMenu.tsx
@@ -10,31 +10,32 @@ import HomeSvg from "../common/icons/home";
 import OrderSvg from "../common/icons/order";
 import RetailerSvg from "../common/icons/retailer";
 import SettingsSvg from "../common/icons/settings";
+import NextLink from "next/link";
 
 export default function SupplierMenu() {
   return (
     <div>
       <Typography variant="h6">Menu</Typography>
       <List>
-        <ListItemButton href="/dashboard">
+        <ListItemButton component={NextLink} href="/dashboard">
           <ListItemIcon>
             <HomeSvg />
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItemButton>
-        <ListItemButton href="/dashboard/orders">
+        <ListItemButton component={NextLink} href="/dashboard/orders">
           <ListItemIcon>
             <OrderSvg />
           </ListItemIcon>
           <ListItemText primary="Orders" />
         </ListItemButton>
-        <ListItemButton href="/dashboard/retailers">
+        <ListItemButton component={NextLink} href="/dashboard/retailers">
           <ListItemIcon>
             <RetailerSvg />
           </ListItemIcon>
           <ListItemText primary="Retailers" />
         </ListItemButton>
-        <ListItemButton href="/dashboard/settings">
+        <ListItemButton component={NextLink} href="/dashboard/settings">
           <ListItemIcon>
             <SettingsSvg />
           </ListItemIcon>
